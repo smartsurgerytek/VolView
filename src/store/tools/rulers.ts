862289443ea004f0745de50a289fcdd9114092d1
winnie.kuo <winnie.kuo@smartsurgerytek.com>
2025-10-23 17:34:48 +0800

    feat(Ruler): implement pixel spacing calculation for ruler length measurement

--- src/store/tools/rulers.ts
+++ src/store/tools/rulers.ts
@@ -1,13 +1,16 @@
 import { computed } from 'vue';
 import { defineAnnotationToolStore } from '@/src/utils/defineAnnotationToolStore';
 import type { Vector3 } from '@kitware/vtk.js/types';
-import { distance2BetweenPoints } from '@kitware/vtk.js/Common/Core/Math';
+// import { distance2BetweenPoints } from '@kitware/vtk.js/Common/Core/Math';
 import { ToolID } from '@/src/types/annotation-tool';
 
 import { RULER_LABEL_DEFAULTS } from '@/src/config';
 import { Manifest, StateFile } from '@/src/io/state-file/schema';
 
+import DicomChunkImage from '@/src/core/streaming/dicomChunkImage';
+import { Tags } from '@/src/core/dicomTags';
 import { useAnnotationTool } from './useAnnotationTool';
+import { useImageCacheStore } from '../image-cache';
 
 const rulerDefaults = () => ({
   firstPoint: [0, 0, 0] as Vector3,
@@ -15,6 +18,30 @@ const rulerDefaults = () => ({
   id: '',
   name: 'Ruler',
 });
+
+function getPixelSpacing(currentImageID: string): number[] {
+    const imageCacheStore = useImageCacheStore();
+
+    const spacing: number[] = [1, 1, 1];
+    const image = imageCacheStore.imageById[currentImageID];
+
+    if (image instanceof DicomChunkImage) {
+        const metaPairs = image.getDicomMetadata();
+        if (metaPairs) {
+            const metadata = Object.fromEntries(metaPairs);
+            const pixelSpacingStr = metadata[Tags.PixelSpacing];
+
+            const pixelSpacing = pixelSpacingStr.split('\\');
+
+            if (pixelSpacing.length > 0) {
+                spacing[0] = parseFloat(pixelSpacing[0])
+                spacing[1] = parseFloat(pixelSpacing[1])
+            }
+        }
+    }
+
+    return spacing;
+}
 
 export const useRulerStore = defineAnnotationToolStore('ruler', () => {
   const annotationTool = useAnnotationTool({
@@ -37,10 +64,21 @@ export const useRulerStore = defineAnnotationToolStore('ruler', () => {
 
   const lengthByID = computed<Record<string, number>>(() => {
     const byID = rulerByID.value;
+
     return rulerIDs.value.reduce((lengths, id) => {
+      const ruler = byID[id];
       const { firstPoint, secondPoint } = byID[id];
+
+      const spacing: number[] = getPixelSpacing(ruler.imageID)
+
+      // turn index space distance to world space
+      const dx = (firstPoint[0] - secondPoint[0]) * spacing[0];
+      const dy = (firstPoint[1] - secondPoint[1]) * spacing[1];
+      const dz = (firstPoint[2] - secondPoint[2]) * spacing[2];
+      const distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
       return Object.assign(lengths, {
-        [id]: Math.sqrt(distance2BetweenPoints(firstPoint, secondPoint)),
+        [id]: distance,
       });
     }, {});
   });

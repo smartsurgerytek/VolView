54e7b5be09f682e42427d1bba34b9e264930b875
Krishtopher Rathod <krishtopher.rathod@faciletechnolab.com>
2025-12-30 13:16:56 +0530

    p6vv
    p6vv

--- server/conversion/settings.py
+++ server/conversion/settings.py
@@ -1,2 +1,17 @@
-DICOMWEB_URL: str = "http://localhost:8080/dicom-web"  
-ABPAPI_URL: str = "https://localhost:44373/api/volview"
+import os
+
+# Localhost Defaults
+DICOMWEB_URL = "http://localhost:8080/dicom-web"
+ABPAPI_URL = "https://localhost:44373/api/volview"
+
+python_env = os.getenv("PYTHON_ENV")
+print(f"DEBUG: PYTHON_ENV is set to: {python_env}")
+
+if python_env == "Integration":
+    try:
+        from .settings_integration import DICOMWEB_URL as CLOUD_DICOM, ABPAPI_URL as CLOUD_ABP
+        DICOMWEB_URL = CLOUD_DICOM
+        ABPAPI_URL = CLOUD_ABP
+        print("DEBUG: Successfully loaded Integration settings")
+    except Exception as e:
+        print(f"DEBUG: Failed to load integration settings. Error: {e}")

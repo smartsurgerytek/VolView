05b57e941a62a11f9c414630e91a27001213c727
Krishtopher Rathod <krishtopher.rathod@faciletechnolab.com>
2025-12-30 13:29:41 +0530

    p7vv
    p7vv

--- server/conversion/settings_integration.py
+++ server/conversion/settings_integration.py
@@ -1,15 +1,2 @@
-import os
-
-# Default Localhost URLs
-DICOMWEB_URL: str = "http://localhost:8080/dicom-web"
-ABPAPI_URL: str = "https://localhost:44373/api/volview"
-
-# Check if we should use Integration settings
-if os.getenv("PYTHON_ENV") == "Integration":
-    try:
-        from .settings_Integration import DICOMWEB_URL as CLOUD_DICOM, ABPAPI_URL as CLOUD_ABP
-        DICOMWEB_URL = CLOUD_DICOM
-        ABPAPI_URL = CLOUD_ABP
-        print("Running with Integration Settings")
-    except ImportError:
-        print("Integration settings file not found, using defaults")
+DICOMWEB_URL: str = "https://idental-orthanc-int-449134413394.asia-east1.run.app/dicom-web"  
+ABPAPI_URL: str = "https://idental-httpapi-host-int-449134413394.asia-east1.run.app/api/volview" 

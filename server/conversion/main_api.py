5b6ee7fc8f10364e3c7e52b11d32d428518e28e1
Krishtopher Rathod <krishtopher.rathod@faciletechnolab.com>
2026-01-13 17:14:36 +0530

    remove server prefix
    remove server prefix

--- server/conversion/main_api.py
+++ server/conversion/main_api.py
@@ -20,9 +20,9 @@ import pydicom
 from PIL import Image
 import base64
 
-from server.models import Manifest
+from models import Manifest
 # from conversion import settings
-from server.conversion import settings
+import settings
 from sr_generator import SRGenerator
 import vtk # 匯入 vtk
 from vtk.util import numpy_support

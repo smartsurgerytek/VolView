a566a40d5d18c9813439925def989a65a4d4c06b
Krishtopher Rathod <krishtopher.rathod@faciletechnolab.com>
2026-01-16 12:58:09 +0530

    replace int url value
    replace int url value

--- server/conversion/settings.py
+++ server/conversion/settings.py
@@ -1,8 +1,10 @@
 import os
 
 # 1. Start with Localhost as the base
-DICOMWEB_URL: str = "http://localhost:8080/dicom-web"
-ABPAPI_URL: str = "https://localhost:44373/api/volview"
+# DICOMWEB_URL: str = "http://localhost:8080/dicom-web"
+# ABPAPI_URL: str = "https://localhost:44373/api/volview"
+DICOMWEB_URL: str = "https://dicom-pacs-int.smartsurgerytek.net/dicom-web"  
+ABPAPI_URL: str = "https://dicom-api-int.smartsurgerytek.net/api/volview" 
 
 print("DEBUG DICOMWEB_URL =", DICOMWEB_URL)
 print("DEBUG ABPAPI_URL =", ABPAPI_URL)
